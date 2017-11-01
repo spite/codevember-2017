2bb815d64b87379a6c6ed7fa3a33dd377ffea557
Jaume Sanchez <the.spite@gmail.com>
2017-11-01 20:04:59 +0000

    scaffolding

---
+++ js/Common.js
@@ -0,0 +1,48 @@
+function addCommonUI( element, callback ) {
+
+	element = element || document.body;
+	callback = callback || function() {}
+
+	function goFS() {
+
+		if(element.requestFullscreen) {
+			element.requestFullscreen();
+		} else if(element.mozRequestFullScreen) {
+			element.mozRequestFullScreen();
+		} else if(element.webkitRequestFullscreen) {
+			element.webkitRequestFullscreen();
+		} else if(element.msRequestFullscreen) {
+			element.msRequestFullscreen();
+		}
+		callback();
+
+	}
+
+	function toggleDetails() {
+		document.getElementById('details').classList.toggle('hidden');
+	}
+
+	window.addEventListener( 'keydown', function( e ) {
+		if( e.keyCode === 70 ) {
+			goFS();
+		}
+	} )
+
+	var btn = document.createElement( 'div' );
+	btn.textContent = '↗';
+	btn.className = 'fullscreen-button button'
+	document.body.appendChild( btn );
+	btn.addEventListener( 'click', goFS );
+
+	var info = document.createElement( 'div' );
+	info.textContent = 'i';
+	info.className = 'info-button button'
+	document.body.appendChild( info );
+	info.addEventListener( 'click', toggleDetails );
+
+	var hub = document.createElement( 'p' );
+	hub.className = 'hub';
+	hub.innerHTML = '<a href="https://clicktorelease.com/code/codevember-2017/" >See other experiments for Codevember 2017</a>';
+	document.body.appendChild( hub );
+
+}
